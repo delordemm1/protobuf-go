9d5c3c94b4995a4c65e429530d2a09486bcfb0af
Emmanuel Ogbonna <46292455+delordemm1@users.noreply.github.com>
2024-07-01 08:59:07 +0100

    feat: gotags

--- cmd/protoc-gen-go/internal_gengo/main.go
+++ cmd/protoc-gen-go/internal_gengo/main.go
@@ -426,7 +426,8 @@ func genMessageField(g *protogen.GeneratedFile, f *fileInfo, m *messageInfo, fie
 	if m.isTracked {
 		tags = append(tags, gotrackTags...)
 	}
-
+	var leftTailing protogen.Comments
+	tags, leftTailing = AppendGoTagsFromFieldComment(tags, field.Comments.Trailing)
 	name := field.GoName
 	if field.Desc.IsWeak() {
 		name = genid.WeakFieldPrefix_goname + name
@@ -437,7 +438,8 @@ func genMessageField(g *protogen.GeneratedFile, f *fileInfo, m *messageInfo, fie
 		field.Desc.Options().(*descriptorpb.FieldOptions).GetDeprecated())
 	g.P(leadingComments,
 		name, " ", goType, tags,
-		trailingComment(field.Comments.Trailing))
+		// trailingComment(field.Comments.Trailing))
+		trailingComment(leftTailing))
 	sf.append(field.GoName)
 }
 
